c6b0deea805684bc7d5c67b3b60cb5b12c4628fd
Adaxry <lyj14282008@gmail.com>
2021-08-31 15:15:14 +0800

    Create eval.sh

---
+++ nmt/evaluation/zhen/eval.sh
@@ -0,0 +1,5 @@
+./delbpe.sh $1
+./detokenizer.perl -u -l en < $1.delbpe > $1.delbpe.detok
+./wrap-xml.perl en newstest2019-zhen-src.zh.sgm test < $1.delbpe.detok > $1.delbpe.detok.sgm
+./mteval-v13a.pl -c -r newstest2019-zhen-ref.en.sgm -s newstest2019-zhen-src.zh.sgm -t $1.delbpe.detok.sgm > $1.eval
+cat $1.eval
