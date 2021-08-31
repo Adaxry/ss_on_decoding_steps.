62d3ea3d6039612d8019f76186c4d1564d31f2ae
Adaxry <lyj14282008@gmail.com>
2021-08-31 15:11:56 +0800

    Update eval.sh

--- nmt/evaluation/enfr/eval.sh
+++ nmt/evaluation/enfr/eval.sh
@@ -1,4 +1,4 @@
 sh ./delbpe.sh $1
 sh ./atat.sh $1
-./multi-bleu.perl ./test.de.tok.atat < ${1}.delbpe.atat > ${1}.delbpe.atat.eval
+./multi-bleu.perl ./test.fr.delbpe.atat < ${1}.delbpe.atat > ${1}.delbpe.atat.eval
 cat ${1}.delbpe.atat.eval  
