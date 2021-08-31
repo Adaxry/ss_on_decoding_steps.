cf3042ca6ba056fc58fe193e53f45aae59ded610
Adaxry <lyj14282008@gmail.com>
2021-08-31 15:16:16 +0800

    Update eval_test18.sh

--- nmt/evaluation/zhen/eval_test18.sh
+++ nmt/evaluation/zhen/eval_test18.sh
@@ -1,4 +1,4 @@
 ./delbpe.sh $1
 ./detokenizer.perl -u -l en < $1.delbpe > $1.delbpe.detok
-./wrap-xml.perl en newstest2018-zhen-src.zh.sgm wc_test < $1.delbpe.detok > $1.delbpe.detok.sgm
+./wrap-xml.perl en newstest2018-zhen-src.zh.sgm test < $1.delbpe.detok > $1.delbpe.detok.sgm
 ./mteval-v13a.pl -c -r newstest2018-zhen-ref.en.sgm -s newstest2018-zhen-src.zh.sgm -t $1.delbpe.detok.sgm > $1.eval 
